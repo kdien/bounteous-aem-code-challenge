e76d22f447ecf258d0bb24490ecc054e0aa3fbb7
Khoa Dien <khoa.dien@hotmail.com>
2021-05-21 00:45:11 -0300

    Add model `User` and custom properties based on API specs

---
+++ core/src/main/java/com/bounteous/aem/challenge/core/models/properties/Coordinates.java
@@ -0,0 +1,25 @@
+package com.bounteous.aem.challenge.core.models.properties;
+
+public class Coordinates {
+
+    public Coordinates() {}
+
+    private String latitude;
+    private String longitude;
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+}
---
+++ core/src/main/java/com/bounteous/aem/challenge/core/models/properties/RegistrationData.java
@@ -0,0 +1,25 @@
+package com.bounteous.aem.challenge.core.models.properties;
+
+public class RegistrationData {
+
+    public RegistrationData() {}
+
+    private String date;
+    private int age;
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
